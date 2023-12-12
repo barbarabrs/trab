1909219aa9c984c1d2009aeab67ea870edd887e4
Barbara <rsouzabarbara@gmail.com>
2023-12-12 01:03:10 -0300

    edicao area logada

--- src/views/AreaLogada.vue
+++ src/views/AreaLogada.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="full-background">
-    <header>
-      <div>
+    <header class="navigation-header">
+      <div class="navigation-buttons">
         <button class="botoes" @click="mostrarFormulario = !mostrarFormulario">
           Nova Playlist
         </button>
@@ -10,21 +10,21 @@
       </div>
     </header>
 
-    <main>
+    <main class="content">
       <div class="center-div">
         <div class="inside-div">
           <!-- Mostrar o formulário para nome da playlist -->
-          <div v-if="mostrarFormulario">
+          <div class="dialog-box dialog-playlist" v-if="mostrarFormulario">
             <input type="text" v-model="novaPlaylist" placeholder="Digite o nome da playlist" />
             <button class="botoes" @click="adicionarNomePlaylist">Continuar</button>
           </div>
-          <!-- Mostrar os botões após adicionar o nome da playlist -->
+<!-- Mostrar os botões após adicionar o nome da playlist -->
           <div v-if="mostrarBotaoSalvar">
             <button class="botoes" @click="salvarPlaylist">Salvar</button>
             <button class="botoes" @click="mostrarFormularioMusica = !mostrarFormularioMusica">Adicionar Música</button>
           </div>
           <!-- Mostrar formulário para adicionar música -->
-          <div v-if="mostrarFormularioMusica">
+          <div class="dialog-box dialog-music" v-if="mostrarFormularioMusica">
             <input type="text" v-model="nomeMusica" placeholder="Nome da música" />
             <input type="text" v-model="artistaMusica" placeholder="Artista" />
             <button class="botoes" @click="adicionarMusica">Salvar Música</button>
@@ -33,6 +33,8 @@
           <ul v-if="mostrarPlaylists">
             <li v-for="(playlist, index) in playlists" :key="index" @click="exibirMusicas(index)">
               {{ playlist.nome }}
+              <button class="botoes" @click="editarPlaylist(index)">Editar</button>
+              <button class="botoes" @click="deletarPlaylist(index)">Deletar</button>
             </li>
           </ul>
           <!-- Exibir músicas da playlist selecionada -->
@@ -43,7 +45,21 @@
                 {{ musica.nome }} - {{ musica.artista }}
               </li>
             </ul>
+
+            <!-- Adicionar música à playlist -->
+            <div>
+              <input type="text" v-model="novaMusica.nome" placeholder="Nome da música" />
+              <input type="text" v-model="novaMusica.artista" placeholder="Artista" />
+              <button class="botoes" @click="adicionarMusica">Adicionar Música</button>
+            </div>
           </div>
+
+          <!-- Mostrar as playlists -->
+          <ul v-if="mostrarPlaylists && playlistSelecionada === null">
+            <li v-for="(playlist, index) in playlists" :key="index" @click="exibirMusicas(index)">
+              {{ playlist.nome }}
+            </li>
+          </ul>
         </div>
       </div>
     </main>
@@ -60,6 +76,11 @@ export default {
       mostrarFormulario: false,
       mostrarBotaoSalvar: false,
       mostrarFormularioMusica: false,
+      musicasToAdd: [],
+      novaMusica: {
+        nome: '',
+        artista: '',
+      },
       nomeMusica: '',
       artistaMusica: '',
       playlistSelecionada: null,
@@ -76,26 +97,76 @@ export default {
           musicas: []
         });
       }
-    },
-    salvarPlaylist() {
-      // Lógica para salvar a playlist
+},
+    
+salvarPlaylist() {
+  if (this.novaPlaylist.trim() !== '') {
+    const playlistExistente = this.playlists.find(playlist => playlist.nome === this.novaPlaylist);
+    if (playlistExistente) {
+      // Playlist com o mesmo nome já existe, não criar outra
       this.mostrarBotaoSalvar = false;
+      this.mostrarFormularioMusica = false;
       this.novaPlaylist = '';
-    },
-    adicionarMusica() {
-      if (this.nomeMusica.trim() !== '' && this.artistaMusica.trim() !== '' && this.playlistSelecionada !== null) {
-        this.playlists[this.playlistSelecionada].musicas.push({
-          nome: this.nomeMusica,
-          artista: this.artistaMusica
+      this.musicasToAdd = [];
+    } else {
+      // Playlist não existe, criar uma nova
+      if (this.musicasToAdd.length > 0) {
+        this.playlists.push({
+          nome: this.novaPlaylist,
+          musicas: this.musicasToAdd.slice() // Copia as músicas para a nova playlist
         });
-        this.nomeMusica = '';
-        this.artistaMusica = '';
-        this.mostrarFormularioMusica = false;
+      } else {
+        this.playlists.push({
+          nome: this.novaPlaylist,
+          musicas: []
+        });
+      }
+
+      this.mostrarBotaoSalvar = false;
+      this.mostrarFormularioMusica = false;
+      this.novaPlaylist = '';
+      this.musicasToAdd = [];
+    }
+  }
+},
+
+adicionarMusica() {
+  if (this.nomeMusica.trim() !== '' && this.artistaMusica.trim() !== '' && this.playlistSelecionada !== null) {
+    this.playlists[this.playlistSelecionada].musicas.push({
+      nome: this.novaMusica.nome,
+      artista: this.novaMusica.artista
+    });
+    this.novaMusica.nome = '';
+    this.novaMusica.artista = ''
+    this.mostrarFormularioMusica = false;
+  }
+  
+
+
+},
+
+    exibirMusicas(index) {
+      console.log('Índice da playlist selecionada:', index);
+      
+      this.playlistSelecionada = index;
+      this.mostrarFormularioMusica = false; // Garante que o formulário de adicionar música esteja oculto
+  },
+  editarPlaylist(index) {
+      // Implemente a lógica para editar a playlist com base no índice fornecido
+      // Você pode abrir um diálogo ou realizar qualquer ação de edição necessária
+      console.log(`Editar playlist ${index}`);
+    },
+
+    deletarPlaylist(index) {
+      // Implemente a lógica para deletar a playlist com base no índice fornecido
+      // Certifique-se de confirmar com o usuário antes de deletar
+      console.log(`Deletar playlist ${index}`);
+      if (confirm("Tem certeza de que deseja deletar esta playlist?")) {
+        this.playlists.splice(index, 1);
       }
     },
-    exibirMusicas(index) {
-      this.playlistSelecionada = index;
-    },
+
+    
     exibirPlaylists() {
       this.mostrarPlaylists = true; // Mostrar as playlists ao clicar no botão "Minhas Playlists"
     }
@@ -120,23 +191,41 @@ export default {
   align-items: center;
 }
 main {
-  background-color: #fff;
+
   background-image: none;
 }
-
-.center-div {
+.navigation-header {
   width: 100%;
-  max-width: 1062px;
-  height: 711px;
-  border-radius: 30px;
-  background: #d9d9d9;
+  background-color: #9E4784;
+  padding: 20px 0;
+  position: relative;
+  z-index: 2;
 }
 
-.inside-div {
-  width: 100%;
-  height: 418px;
-  border-radius: 0px 0px 20px 20px;
-  background: #2d2d2d;
+
+.navigation-buttons {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+.dialog-box {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+  z-index: 3; /* Para ficar acima do restante do conteúdo */
+}
+
+.dialog-playlist {
+  
+}
+
+.dialog-music {
+  /* Estilos específicos para a caixa de diálogo de adição de música */
 }
 
 .botoes {
